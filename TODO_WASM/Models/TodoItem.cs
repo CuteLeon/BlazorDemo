dd071bc5f41868a884a99cdaa255cc4bbb24e556
Leon <Leon.Engineer@hotmail.com>
2020-08-02 19:43:29 +0800

    实现MSDN的WASM例子；Document；

---
+++ TODO_WASM/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿namespace TODO_WASM.Models
+{
+    public class TodoItem
+    {
+        public TodoItem()
+        {
+        }
+
+        public TodoItem(string title, bool done = false)
+            : this()
+        {
+            Title = title;
+            Done = done;
+        }
+
+        public string Title { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
